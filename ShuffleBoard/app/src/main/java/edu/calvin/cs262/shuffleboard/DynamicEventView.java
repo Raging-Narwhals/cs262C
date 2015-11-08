bbe692441b7d9c15f7492a7a5b7330bb63a2482e
Spencer Schultz <srs27@students.calvin.edu>
2015-11-08 14:01:51 -0500

    Made a ListView for schedule view
    Hard-coded demo data

--- ShuffleBoard/app/src/main/java/edu/calvin/cs262/shuffleboard/DynamicEventView.java
+++ ShuffleBoard/app/src/main/java/edu/calvin/cs262/shuffleboard/DynamicEventView.java
@@ -1,6 +1,8 @@
 package edu.calvin.cs262.shuffleboard;
 
 /**
+ * Class corresponding to the UI page for viewing dynamic events; schedule view
+ *
  * Created by Spencer Schultz on 11/6/2015.
  */
 
@@ -10,7 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class DynamicEventView extends AppCompatActivity {
 
@@ -18,15 +22,25 @@ public class DynamicEventView extends AppCompatActivity {
     Button goToStaticEventButton;
 
 
+    //Hard-coded data for demo, delete once data creation and storing is implemented
+    String tempEvent1 = "Grading \nDuration: 1 Hour \nTimes Per Week: 2";
+    String tempEvent2 = "Work out \nDuration: 0.5 Hour \nTimes Per Week: 3";
+    String tempEvent3 = "Slay Bears \nDuration: 1.5 Hour \nTimes Per Week: 1";
+    String[] tempEvents = {tempEvent1, tempEvent2, tempEvent3};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dynamic_event_view);
 
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        //align button object with UI buttons
         createDynamicEventButton = (Button) findViewById(R.id.createDynamicEventButton);
         goToStaticEventButton = (Button) findViewById(R.id.goToStaticEventsButton);
 
+        //when button is clicked, send user to UI page for creating a dynamic event
         createDynamicEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +49,7 @@ public class DynamicEventView extends AppCompatActivity {
             }
         });
 
+        //when button is clicked, send user to UI page for viewing static events
         goToStaticEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +57,15 @@ public class DynamicEventView extends AppCompatActivity {
                 startActivityForResult(myIntent, 0);
             }
         });
+
+        //Set adapter with the string array of events from saved data, input them into list
+        ArrayAdapter<String> myAdapter=new
+                ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                tempEvents);
+        listView.setAdapter(myAdapter);
+
     }
 
     @Override
--- ShuffleBoard/app/src/main/java/edu/calvin/cs262/shuffleboard/StaticEventView.java
+++ ShuffleBoard/app/src/main/java/edu/calvin/cs262/shuffleboard/StaticEventView.java
@@ -1,16 +1,20 @@
 package edu.calvin.cs262.shuffleboard;
 
+
 /**
+ * Class corresponding to the UI page for viewing static events; schedule view
+ *
  * Created by Spencer Schultz on 11/6/2015.
  */
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class StaticEventView extends AppCompatActivity {
 
@@ -18,15 +22,24 @@ public class StaticEventView extends AppCompatActivity {
     Button goToDynamicEventButton;
 
 
+    //Hard-coded data for demo, delete once data creation and storing is implemented
+    String tempEvent1 = "Soccer Practice \n4:00PM - 5:00PM \nOn: Monday, Wednesday, Friday";
+    String tempEvent2 = "Unicycle Club \n7:00PM - 9:00PM \nOn: Tuesday";
+    String tempEvent3 = "Tutoring \n11:00AM - 12:30PM || \nMonday, Thursday";
+    String[] tempEvents = {tempEvent1, tempEvent2, tempEvent3};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_static_event_view);
 
+        ListView listView = (ListView) findViewById(R.id.listView2);
+
+        //align button object with UI buttons
         createStaticEventButton = (Button) findViewById(R.id.createStaticEventButton);
         goToDynamicEventButton = (Button) findViewById(R.id.goToDynamicEventsButton);
 
+        //when button is clicked, send user to UI page for creating static events
         createStaticEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +48,7 @@ public class StaticEventView extends AppCompatActivity {
             }
         });
 
+        //when button is clicked, send user to UI page for viewing dynamic events
         goToDynamicEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +56,15 @@ public class StaticEventView extends AppCompatActivity {
                 startActivityForResult(myIntent, 0);
             }
         });
+
+        //Set adapter with the string array of events from saved data, input them into list
+        ArrayAdapter<String> myAdapter=new
+                ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                tempEvents);
+        listView.setAdapter(myAdapter);
+
     }
 
     @Override
